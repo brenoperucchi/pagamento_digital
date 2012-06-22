4cd251b0bdcb237b383fe656c77aa2835edd3848
Breno Perucchi <breno@imentore.com.br>
2012-06-22 16:29:46 -0300

    modified:   app/views/pagamento_digital/_pagamento_digital_form.html.erb 	modified:   lib/pagamento_digital/base.rb

--- lib/pagamento_digital/base.rb
+++ lib/pagamento_digital/base.rb
@@ -1,7 +1,15 @@
 # encoding: UTF-8
 
 module PagamentoDigital
+  attr_accessor :token, :email, :return_to
   extend self
+
+  def config_pd
+    self.token ||= '9CA8D46AF0C6177CB4C23D76CAF5E4B0'
+    self.email ||= 'user@example.com'
+    self.return_to ||= 'http://exemplo.com.br/pedido/efetuado'
+    @@config = {'authenticity_token'=> self.token, 'email' => self.email, 'return_to' => self.return_to }
+  end
 
   # O Pagamento Digital recebe todos os pedidos nesta URL. Caso o modo desenvolvedor esteja
   # ativado então a URL será /pagamento_digital/checkout
@@ -12,29 +20,32 @@ module PagamentoDigital
 
   # Caminho para o arquivo de configuração
   def config_file
-    Rails.root.join("config/pagamento_digital.yml")
+    # Rails.root.join("config/pagamento_digital.yml")
   end
 
   # Verifica se o arquivo de configuração existe
   def config?
-    File.exist?(config_file)
+    # File.exist?(config_file)
+    true
   end
 
   # Carrega o arquivo de configuração.
   def config
-    raise MissingConfigurationError, "arquivo não encontrado em #{config_file.inspect}" unless config?
+    # binding.pry
+    # raise MissingConfigurationError, "arquivo não encontrado em #{config_file.inspect}" unless config?
 
     # Carrega arquivo caso o mesmo não esteja carregado
-    @@config ||= YAML.load_file(config_file)
+    # @@config ||= YAML.load_file(config_file)
 
     # Lança uma exceção se o ambiente não foi definidor
     # ou se o arquivo está vazio
-    if @@config == false || !@@config[Rails.env]
-      raise MissingEnvironmentError, ":ambiente #{Rails.env} não definido em #{config_file.inspect}"
-    end
+    # if @@config == false || !@@config[Rails.env]
+    #   raise MissingEnvironmentError, ":ambiente #{Rails.env} não definido em #{config_file.inspect}"
+    # end
 
     # Obtém as configurações do ambiente
-    @@config[Rails.env]
+    # @@config[Rails.env]
+    config_pd
   end
 
   # A URL apontará para uma URL local caso a aplicação
